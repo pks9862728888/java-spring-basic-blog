c4cc5c4d65c6688f602fb4734577552faf34aff8
Pran Kumar Sarkar <pks9862728888@gmail.com>
2022-03-08 21:25:50 +0530

    Created a controller that displays a Thymeleaf Template

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -2,7 +2,6 @@ package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
